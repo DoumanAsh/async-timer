e2b81b3851a1c870e6f586823b9a141727726350
Douman <douman@gmx.se>
2020-06-03 13:21:57 +0200

    Warn when doing actual cross compilation

--- build.rs
+++ build.rs
@@ -1,7 +1,21 @@
+
 fn main() {
-    #[cfg(all(feature = "c_wrapper", unix, not(any(target_os = "macos", target_os = "ios"))))]
+    #[cfg(feature = "c_wrapper")]
     {
-        cc::Build::new().file("src/c_wrapper/posix.c")
-                        .compile("libposix_wrapper.a");
+        use std::env;
+
+        let host = env::var("HOST").unwrap();
+        let target = env::var("TARGET").unwrap();
+
+        if host != target {
+            println!("cargo:warning=async-timer is cross-compiled, C wrapper cannot be used. Sorry but I'm too lazy to bother with it, use cross or docker with proper image");
+            return;
+        }
+
+        #[cfg(all(target_family = "unix", not(any(target_os = "macos", target_os = "ios"))))]
+        {
+            cc::Build::new().file("src/c_wrapper/posix.c")
+                            .compile("libposix_wrapper.a");
+        }
     }
 }
